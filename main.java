4478fbf5ebc8ebabb53509b9ffd0d3024f74d7fa
eliel358 <elielhenrique360flip@gmail.com>
2024-03-17 08:59:16 -0300

    Checando vencedores

--- main.java
+++ main.java
@@ -1,32 +1,50 @@
 import java.util.Scanner;
 class main{
+        public static void show_game(String[] pos){
+                System.out.println(" "+pos[0]+"|"+pos[1]+"|"+pos[2]);
+                System.out.println("-------");
+                System.out.println(" "+pos[3]+"|"+pos[4]+"|"+pos[5]);
+                System.out.println("-------");
+                System.out.println(" "+pos[6]+"|"+pos[7]+"|"+pos[8]);
+        }
+        public static void check_winner(String[] pos){
+                if(pos[0].equals(pos[1]) && pos[0].equals(pos[2]) && pos[0].equals("X") ||
+                   pos[3].equals(pos[4]) && pos[3].equals(pos[5]) && pos[3].equals("X") ||
+                   pos[6].equals(pos[7]) && pos[6].equals(pos[8]) && pos[6].equals("X") ||
+                   pos[0].equals(pos[4]) && pos[0].equals(pos[8]) && pos[0].equals("X") ||
+                   pos[2].equals(pos[4]) && pos[2].equals(pos[6]) && pos[2].equals("X")){
+                                System.out.println("X ganhou !!!");
+                }else if(
+                   pos[0].equals(pos[1]) && pos[0].equals(pos[2]) && pos[0].equals("O") ||
+                   pos[3].equals(pos[4]) && pos[3].equals(pos[5]) && pos[3].equals("O") ||
+                   pos[6].equals(pos[7]) && pos[6].equals(pos[8]) && pos[6].equals("O") ||
+                   pos[0].equals(pos[4]) && pos[0].equals(pos[8]) && pos[0].equals("O") ||
+                   pos[2].equals(pos[4]) && pos[2].equals(pos[6]) && pos[2].equals("O")){
+                                System.out.println("O ganhou !!!");
+                }
 
-	public static void show_game(char[] pos){
-		System.out.println(positions);
-	}	
+        }
+        public static void main(String[] args){
+                Scanner scan = new Scanner(System.in);
+                String[] positions = {"0","0","0","0","0","0","0","0","0"};
+                int turn = 1;
+                while(true){
+                        if(turn == 1){
+                                show_game(positions);
+                                System.out.println("Onde quer jogar? ");
+                                int play = scan.nextInt();
+                                positions[play] = "X";
+                                turn = 2;
+                                check_winner(positions);
+                        }else if(turn == 2){
+                                show_game(positions);
+                                System.out.println("Onde quer jogar? ");
+                                int play = scan.nextInt();
+                                positions[play] = "O";
+                                turn = 1;
+                                check_winner(positions);
+                        }
 
-
-	public static void main(String[] args){
-	char[] positions = new char[9];
-	byte turn = 1;
-	Scanner scan = new Scanner(System.in);
-	
-	while(true){
-		if(turn == 1){
-	
-			System.out.println("Onde você quer jogar 1 ");
-			var play = scan.nextLine();	
-			turn = 2;
-	
-		}else if(turn == 2){
-	
-			System.out.println("Onde você que jogar 2 ");
-			var play = scan.nextLine();
-			turn = 1;
-			show_game();
-		
-		}
-	}
-
-	}
+                }
+        }
 }
